ae8ed4f2f2ac53f7360f4b89ee557a70c2912a0f
Lavi26 <89968738+Lavi26@users.noreply.github.com>
2021-10-18 15:43:29 +0530

    Add files via upload

---
+++ ECommerceAPI-UI/EcommerceApi/src/main/java/com/spring/demo/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.spring.demo.service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.spring.demo.model.Product;
+
+public interface ProductService {
+
+	    //add for product
+		Product addProduct(Product product);
+		
+		//List of all products
+		ArrayList<Product> ListOfProducts();
+		
+		//Delete of Product
+		void deleteProductById(Integer id);
+		
+		//Update product
+		public Product updateProduct(Product product);
+}
